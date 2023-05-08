242d25b37f7777b5fa7e394f74a61d4d372b1109
Katerina <yelkaterina@gmail.com>
2023-05-08 20:29:44 +0200

    Tidy-ups

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,3 @@
-const API_URL = 'https://ci-jshint.herokuapp.com/api';
 const resultsModal = new bootstrap.Modal(document.getElementById('resultsModal'));
 
 document.getElementById('status').addEventListener('click', e => getStatus(e));
